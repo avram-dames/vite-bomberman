ffa5a1620289616c66467568cb8db38c7a267b68
Avram Dames <avram.dames@gmail.com>
2024-01-11 14:16:23 +0200

    remove log

--- src/state/game.test.ts
+++ src/state/game.test.ts
@@ -6,7 +6,6 @@ import { expect, test } from "vitest";
 import game from "./game";
 
 test("Game clock ticks", () => {
-  console.log("game", game);
   const currentTime = game.data.clock;
   game.tick();
   expect(game.data.clock).toBe(currentTime + 1);
